895cf7c9245ed5a1ef6744d6d414e63b918b7582
Betelhemmesele <betimesele93@gmail.com>
2023-11-17 23:58:19 +0300

    connectd to mongodb database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.mongo).then(()=>{
+    console.log('connectd to db');
+}).catch((err)=>{
+  console.log(err);
+});
+
 const app = express();
 app.listen(4000,() => {
     console.log("listening on 4000");
