2e0680a33ebea16b13389c3361e236ec194473f1
Betelhemmesele <betimesele93@gmail.com>
2023-11-18 15:34:15 +0300

    seeting up models and controllers

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.routes.js';
 dotenv.config();
 mongoose.connect(process.env.mongo).then(()=>{
     console.log('connectd to db');
@@ -12,3 +13,4 @@ const app = express();
 app.listen(4000,() => {
     console.log("listening on 4000");
 });
+app.use('/api/user',userRouter);
